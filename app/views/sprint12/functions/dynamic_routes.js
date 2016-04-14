e4a063c52a3363e05587da7d0e2c5a62cfd45cbe
mikeycattell <michael.a.cattell@gmail.com>
2016-04-14 15:17:44 +0100

    adding files for new repo
    Taken over from craig, adding new paths for housing benefits.

---
+++ app/views/sprint12/functions/dynamic_routes.js
@@ -0,0 +1,167 @@
+var dynamic_routes = {
+  urls: function(req, res) {
+    var urls = {};
+    var fraud_urls = {};
+
+
+    // Details
+    if (req.path === '/details') {
+      if (req.body.name && req.body.address && req.body.age ||
+          req.body.nino && req.body.name ||
+          req.body.nino && req.body.address ||
+          req.body.nino && req.body.age)
+      {
+        urls.next = '/sprint12/details-more';
+      } else {
+        urls.next = '/sprint12/exit';
+      }
+      urls.back = '/sprint12/start-page';
+    }
+
+    // Fraud Type
+    if (req.path === '/fraud-type') {
+      fraud_urls = req.body;
+
+      if ( fraud_urls.employment === "true" ) {
+        urls.next = 'employment-details';
+      } else if ( fraud_urls.partner === "true" ) {
+        urls.next = 'partner-details';
+      } else if ( fraud_urls.disability === "true" ) {
+        urls.next = 'disability';
+      } else if ( fraud_urls.carers === "true" ) {
+        urls.next = 'carers';
+      } else if ( fraud_urls.abroad === "true" ) {
+        urls.next = 'abroad';
+      } else if ( fraud_urls.identity === "true" ) {
+        urls.next = 'identity';
+      } else if ( fraud_urls.income === "true" ) {
+        urls.next = 'income';
+      } else if ( fraud_urls.other === "true" ) {
+        urls.next = 'other';
+      }
+    }
+
+    // Employment Details
+    if (req.path === '/employment-details') {
+      fraud_urls = req.cookies.fraud_urls;
+
+      if ( fraud_urls.partner === "true" ) {
+        urls.next = 'partner-details';
+      } else if ( fraud_urls.disability === "true" ) {
+        urls.next = 'disability';
+      } else if ( fraud_urls.carers === "true" ) {
+        urls.next = 'carers';
+      } else if ( fraud_urls.abroad === "true" ) {
+        urls.next = 'abroad';
+      } else if ( fraud_urls.identity === "true" ) {
+        urls.next = 'identity';
+      } else if ( fraud_urls.income === "true" ) {
+        urls.next = 'income';
+      } else if ( fraud_urls.other === "true" ) {
+        urls.next = 'other';
+      } else {
+        urls.next = 'additional-information';
+      }
+    }
+
+    // Partner Employment
+    if (req.path === '/partner-employment') {
+      fraud_urls = req.cookies.fraud_urls;
+
+      if ( fraud_urls.disability === "true" ) {
+        urls.next = 'disability';
+      } else if ( fraud_urls.carers === "true" ) {
+        urls.next = 'carers';
+      } else if ( fraud_urls.abroad === "true" ) {
+        urls.next = 'abroad';
+      } else if ( fraud_urls.identity === "true" ) {
+        urls.next = 'identity';
+      } else if ( fraud_urls.income === "true" ) {
+        urls.next = 'income';
+      } else if ( fraud_urls.other === "true" ) {
+        urls.next = 'other';
+      } else {
+        urls.next = 'additional-information';
+      }
+    }
+
+    // Disability
+    if (req.path === '/disability') {
+      fraud_urls = req.cookies.fraud_urls;
+
+      if ( fraud_urls.carers === "true" ) {
+        urls.next = 'carers';
+      } else if ( fraud_urls.abroad === "true" ) {
+        urls.next = 'abroad';
+      } else if ( fraud_urls.identity === "true" ) {
+        urls.next = 'identity';
+      } else if ( fraud_urls.income === "true" ) {
+        urls.next = 'income';
+      } else if ( fraud_urls.other === "true" ) {
+        urls.next = 'other';
+      } else {
+        urls.next = 'additional-information';
+      }
+    }
+
+    // Carers More
+    if (req.path === '/carers-more') {
+      fraud_urls = req.cookies.fraud_urls;
+
+      if ( fraud_urls.abroad === "true" ) {
+        urls.next = 'abroad';
+      } else if ( fraud_urls.identity === "true" ) {
+        urls.next = 'identity';
+      } else if ( fraud_urls.income === "true" ) {
+        urls.next = 'income';
+      } else if ( fraud_urls.other === "true" ) {
+        urls.next = 'other';
+      } else {
+        urls.next = 'additional-information';
+      }
+    }
+
+    // Abroad
+    if (req.path === '/abroad') {
+      fraud_urls = req.cookies.fraud_urls;
+
+      if ( fraud_urls.identity === "true" ) {
+        urls.next = 'identity';
+      } else if ( fraud_urls.income === "true" ) {
+        urls.next = 'income';
+      } else if ( fraud_urls.other === "true" ) {
+        urls.next = 'other';
+      } else {
+        urls.next = 'additional-information';
+      }
+    }
+
+    // Identity
+    if (req.path === '/identity') {
+      fraud_urls = req.cookies.fraud_urls;
+
+      if ( fraud_urls.income === "true" ) {
+        urls.next = 'income';
+      } else if ( fraud_urls.other === "true" ) {
+        urls.next = 'other';
+      } else {
+        urls.next = 'review';
+      }
+    }
+
+    // Income More
+    if (req.path === '/income-more') {
+      fraud_urls = req.cookies.fraud_urls;
+
+      if ( fraud_urls.other === "true" ) {
+        urls.next = 'other';
+      } else {
+        urls.next = 'additional-information';
+      }
+    }
+
+    return urls;
+  },
+};
+
+module.exports = dynamic_routes;
---
+++ server.js
@@ -0,0 +1,113 @@
+var path = require('path'),
+    express = require('express'),
+    nunjucks = require('express-nunjucks'),
+    routes = require(__dirname + '/app/routes.js'),
+    favicon = require('serve-favicon'),
+    app = express(),
+    basicAuth = require('basic-auth'),
+    bodyParser = require('body-parser'),
+    config = require(__dirname + '/app/config.js'),
+    port = (process.env.PORT || config.port),
+    utils = require(__dirname + '/lib/utils.js'),
+    cookieParser = require('cookie-parser'),
+
+    // Custom route directories
+    sprint10 = require(__dirname + '/app/routes/sprint10.js'),
+    sprint11 = require(__dirname + '/app/routes/sprint11.js'),
+    sprint12 = require(__dirname + '/app/routes/sprint12.js'),
+    sprint13 = require(__dirname + '/app/routes/sprint13.js'),
+
+
+
+// Grab environment variables specified in Procfile or as Heroku config vars
+    username = process.env.USERNAME,
+    password = process.env.PASSWORD,
+    env      = process.env.NODE_ENV || 'development',
+    useAuth  = process.env.USE_AUTH || config.useAuth;
+
+// Authenticate against the environment-provided credentials if running
+// the app in production (Heroku, effectively)
+if (env === 'production' && useAuth === 'true'){
+    app.use(utils.basicAuth(username, password));
+}
+
+// Application settings
+app.set('view engine', 'html');
+app.set('views', [__dirname + '/app/views', __dirname + '/lib/']);
+
+nunjucks.setup({
+  autoescape: true,
+  watch: true,
+  noCache: true
+}, app);
+
+// Middleware to serve static assets
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/govuk_modules/govuk_template/assets'));
+app.use('/public', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit'));
+app.use('/public/images/icons', express.static(__dirname + '/govuk_modules/govuk_frontend_toolkit/images'));
+
+// Elements refers to icon folder instead of images folder
+app.use(favicon(path.join(__dirname, 'govuk_modules', 'govuk_template', 'assets', 'images','favicon.ico')));
+
+// Support for parsing data in POSTs
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// send assetPath to all views
+app.use(function (req, res, next) {
+  res.locals.asset_path="/public/";
+  next();
+});
+
+// Add variables that are available in all views
+app.use(function (req, res, next) {
+  res.locals.serviceName=config.serviceName;
+  next();
+});
+
+// routes (found in app/routes.js)
+if (typeof(routes) != "function"){
+  console.log(routes.bind);
+  console.log("Warning: the use of bind in routes is deprecated - please check the prototype kit documentation for writing routes.");
+  routes.bind(app);
+} else {
+  app.use("/", routes);
+}
+
+// Custom routes
+app.use("/sprint10", sprint10);
+app.use("/sprint11", sprint11);
+app.use("/sprint12", sprint12);
+app.use("/sprint13", sprint13);
+
+// auto render any view that exists
+app.get(/^\/([^.]+)$/, function (req, res) {
+
+  var path = (req.params[0]);
+
+  res.render(path, function(err, html) {
+    if (err) {
+      res.render(path + "/index", function(err2, html) {
+        if (err2) {
+          console.log(err);
+          res.status(404).send(err + "<br>" + err2);
+        } else {
+          res.end(html);
+        }
+      });
+    } else {
+      res.end(html);
+    }
+  });
+});
+
+// start the app
+
+app.listen(port);
+console.log('');
+console.log('Listening on port ' + port);
+console.log('');
